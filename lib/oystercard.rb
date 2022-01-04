639ded1c82bc3780ba73fa9a0910fcbe241f2154
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-04 18:37:30 +0100

    finished challenge #7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,21 @@
 class Oystercard
 
-  attr_reader :balance 
- 
+  MAX_BALANCE = 90
+
+  attr_reader :balance
+
   def initialize()
     @balance = 0
   end  
 
   def top_up!(value)
+    raise "Limit reached. Limit is #{MAX_BALANCE}!" if (@balance + value) > MAX_BALANCE
+
     @balance += value
+  end
+
+  def deduct!(value)
+    @balance -= value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,13 +3,25 @@ require "oystercard"
 
 describe Oystercard do
 
-  it "new Oystercard has a balance of zero" do
+  it "Fresh Oystercard has a balance of zero" do
     expect(subject.balance).to eq(0)
   end
 
   describe '#top_up!' do
-    it 'adds money to the oystercard' do
-      expect { subject.top_up!(10) }.to change{ subject.balance }.by 10
+    it 'Adds money to the Oystercard' do
+      expect { subject.top_up!(10) }.to change{ subject.balance }.by(10)
+    end
+    it 'Throws an error when a certain limit reached' do
+      limit = Oystercard::MAX_BALANCE
+      subject.top_up!(limit)
+      expect { subject.top_up!(1)}.to raise_error("Limit reached. Limit is #{limit}!")
+    end
+  end
+
+  describe '#deduct!' do
+    it 'Deducts money from the Oystercard' do
+      subject.top_up!(20)
+      expect { subject.deduct!(10) }.to change {subject.balance}.by(-10)
     end
   end
 
