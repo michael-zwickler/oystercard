129a1404e5ad178e167f68c34855dd5a9bca8199
Viktor Vrecko <vvrecko@posteo.de>
2022-01-04 17:10:09 +0100

    04_adding_balance.md

---
+++ lib/oystercard.rb
@@ -0,0 +1,10 @@
+class Oystercard
+ 
+  def initialize
+    @balance = 0
+  end  
+
+  attr_reader :balance  
+
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+# require "./lib/oystercard.rb" #if you dont use lib as folder
+require "oystercard"
+
 describe Oystercard do
+
+  it "new Oystercard has a balance of zero" do
+  expect(subject.balance).to eq(0)
+  end
 
 end
